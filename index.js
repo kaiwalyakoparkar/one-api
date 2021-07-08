65bd81bd4e66d4c4bc5b66d1653b8fb551575b90
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-07-08 12:49:09 +0530

    fix: :rocket: Home endpoint shifted

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', require(path.join(__dirname, './routes/route')));
+app.use('/api/all', require(path.join(__dirname, './routes/route')));
 
 app.listen(port, () => {
     console.log(`Started server on http://localhost:${port}`);
