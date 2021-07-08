b2cd97de975fe696fe6bb55eed8f6b1bf30b51fe
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-07-08 13:14:09 +0530

    fix: :rocket: Home endpoint added

--- routes/route.js
+++ routes/route.js
@@ -9,11 +9,11 @@ router.get('/', (req, res) => {
     res.send(`<h1>head over to <a href="/api/all">all data</></h1>`);
 });
 router.get('/api/all', (req, res) => {
-    res.status(200).json(data);
+    res.status(200).send(data);
 });
 
 router.get('/api/student/all', (req, res) => {
-    res.status(200).json(data.student);
+    res.status(200).send(data.student);
 });
 
 router.get('/api/student/name/:name', (req, res) => {
@@ -21,14 +21,14 @@ router.get('/api/student/name/:name', (req, res) => {
         return e.name == req.params.name;
     });
 
-    res.status(200).json(MyStudent);
+    res.status(200).send(MyStudent);
 });
 
 router.get('/api/student/rollno/:roll', (req, res) => {
     const MyStudent = data.student.filter((e) => {
         return e.rollno == parseInt(req.params.roll);
     });
-    res.status(200).json(MyStudent);
+    res.status(200).send(MyStudent);
 });
 
 router.get('/api/student/result/:pass', (req, res) => {
@@ -36,11 +36,11 @@ router.get('/api/student/result/:pass', (req, res) => {
         return e.pass == Boolean(req.params.pass);
     });
 
-    res.status(200).json(MyStudents);
+    res.status(200).send(MyStudents);
 });
 
 router.get('/api/teacher/all', (req, res) => {
-    res.status(200).json(data.teacher);
+    res.status(200).send(data.teacher);
 });
 
 router.get('/api/teacher/name/:name', (req, res) => {
@@ -48,14 +48,14 @@ router.get('/api/teacher/name/:name', (req, res) => {
         return e.name == req.params.name;
     });
 
-    res.status(200).json(myTeacher);
+    res.status(200).send(myTeacher);
 });
 
 router.get('/api/teacher/id/:id', (req, res) => {
     const myTeacher = data.teacher.filter((e) => {
         return e.id == parseInt(req.params.id);
     });
-    res.status(200).json(myTeacher[0]);
+    res.status(200).send(myTeacher[0]);
 });
 
 router.get('/api/teacher/subject/:subject', (req,res) => {
@@ -63,7 +63,7 @@ router.get('/api/teacher/subject/:subject', (req,res) => {
         return e.subject == req.params.subject;
     });
 
-    res.status(200).json(myTeacher);
+    res.status(200).send(myTeacher);
 });
 
 module.exports = router;
