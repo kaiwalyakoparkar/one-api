d94af301d24d970cbac13f46f6d92964eeab8789
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-07-08 12:42:49 +0530

    fix: :rocket: Port linked with environment for deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express  = require('express');
 const path = require('path');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use('/', require(path.join(__dirname, './routes/route')));
 
