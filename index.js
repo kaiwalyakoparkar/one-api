2d2e37c84f0bc4045bdcd86cb056b7d3fddcd762
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-08-02 09:38:19 +0530

    fix: :hammer: Fixed the fs module with path module

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const app = express();
 
 const port = 5000;
 
-// app.get('/',(req, res) => {
-// 	res.send('Hey, How are you?');
-// });
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = require(path.join(__dirname, './dev-data/data/tours-simple.json'));
 
 app.get('/api/v1/tours', (req, res) => {
 	res.json({
